44304e01817a272206e9cbfc7bd8fec1cf3c4feb
ARElton <ameliareneeelton@gmail.com>
2021-01-22 15:32:22 -0500

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all 
+Pattern.destroy_all 
+Project.destroy_all 
+
+roy = User.create(username: "Roy")
+amelia = User.create(username: "Amelia")
+aaron = User.create(username: "Aaron")
+
+Pattern.create(image: "", name: "Girl Picking Gourds", category: "Tiffany Style", description: "Original was made in 1890 for a living room at Larelton Hall, Oyster Bay, NY.")
+Pattern.create(image: "", name: "Autumn", category: "Tiffany Style", description: "designed by Lydia Emmet in 1893")
+Pattern.create(image: "", name: "Bird in Flight", category: "Art Deco Style", description: "Window pane with a geometric bird flying in front of a sunset.")
+Pattern.create(image: "", name: "Two Roses", category: "Art Nouveau Style", description: "Square panel with two intertwined roses")
+
+5.times do 
+    Project.create(user_id: User.all.sample.id, pattern_id: Pattern.all.sample.id, image: null, favorite: true)
+end
